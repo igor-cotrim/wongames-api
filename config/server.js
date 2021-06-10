b5200935c8d59d05403852ca2c9e508f170a2462
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-06-10 20:59:11 -0300

    .

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '69ecb71b08ddb8c4f695425757e65d44'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '66e5a189-0d0f-4cc0-922f-b447e16e4924'
+};
